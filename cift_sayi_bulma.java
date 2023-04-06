1994fbac3474d510fec71c6af633e7b63a1d38bf
rabia eda <116549390+ryuveda@users.noreply.github.com>
2023-04-06 13:55:57 +0300

    Add files via upload

---
+++ cift_sayi_bulma.java
@@ -0,0 +1,25 @@
+package patika;
+import java.util.Scanner;
+public class cift_sayi_bulma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter a number \n");
+		int number = scan.nextInt();
+		int sum=0;
+		int total=0;
+		while(i<=number)
+		{
+			if(i%3==0 && i%4==0)
+			{
+				sum=sum+i;
+				total++;
+				System.out.print(i+" ");
+			}				
+			i++;
+		}
+		System.out.println("\nOrtalama :"+sum/total);
+	}
+}
